70841a5a77dcf828d5424731a3a1cf37c5ebe866
marekgacek45 <marekgacek45@gmail.com>
2023-06-12 08:25:16 +0200

    initial

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Shop</title>
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
+
+
+<body>
+   <p>test</p>
+</body>
+</html>
