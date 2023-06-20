cef446c1161677e3a5aa30f0e3e858540ac4c78a
marekgacek45 <marekgacek45@gmail.com>
2023-06-20 08:04:57 +0200

    create new navbar

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,11 +1,11 @@
-<nav class="nav">
+{{-- <nav class="nav">
     <div class="nav__container">
         <div style="display:flex">
 
 
             <a href="{{ route('home') }}">
                 <div class="nav__logo-container">
-                    {{-- <img src="../dist/img/logo.png" alt="logo" class="nav__logo-img"> --}}
+                 
 
                     <h2 class="nav__logo-title">Online Shop</h2>
                 </div>
@@ -42,5 +42,36 @@
             </button>
         </div>
 
+    </div>
+</nav> --}}
+
+
+<nav class="navbar">
+    <div class="navbar__logo">
+        <a href="{{ route('home') }}">TrendSetter</a>
+    </div>
+    <ul class="navbar__links">
+        <li><a href=""></a>Item1</li>
+        <li><a href=""></a>Item2</li>
+        <li><a href=""></a>Item3</li>
+    </ul>
+    <ul class="navbar__actions">
+        <li><i class="uil uil-heart"></i></li>
+        <li><i class="uil uil-shopping-cart-alt"></i></li>
+    </ul>
+    <button class="hamburger hamburger--boring navbar__hamburger" type="button">
+        <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+        </span>
+    </button>
+
+    <div class="navbar__dropdown-menu">
+        <li><a href=""></a>Item1</li>
+        <li><a href=""></a>Item2</li>
+        <li><a href=""></a>Item3</li>
+        <div class="navbar__dropdown-menu__actions">
+            <li><i class="uil uil-heart"></i></li>
+            <li><i class="uil uil-shopping-cart-alt"></i></li>
+        </div>
     </div>
 </nav>
