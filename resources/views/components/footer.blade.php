0a6550346ea99e743f2eef9e38c095cc7f5ddfb4
marekgacek45 <marekgacek45@gmail.com>
2023-06-29 08:21:36 +0200

    add footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,5 @@
-<footer>
-    <hr>
-    <p>Strona wykonana przez <a href="https://gorskastrona.pl" target="_blank">Górska Strona</a></p>
+<footer >
+    
+    <div class="footer">
+    <p>Strona wykonana przez <a href="https://gorskastrona.pl" target="_blank">Górska Strona </a></p><img src="{{asset('images/avatar_logo.png')}}" alt="" style="width:30px"></div>
 </footer>
